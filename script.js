2ed7747d84628e0db39b50e9472634c21fcc97bd
MadMilliner <jasonapeterson@gmail.com>
2025-07-07 17:25:37 -0700

    It's there! Just needs a touch of cleanup

--- script.js
+++ script.js
@@ -1,61 +1,101 @@
-var gameboard = {
-  One: "",
-  Two: "",
-  Three: "",
-  Four: "",
-  Five: "",
-  Six: "",
-  Seven: "",
-  Eight: "",
-  Nine: "",
-}
+// players object
 
 var players = {
-    player1: {name: window.prompt("player1","player1").valueOf, marker: "X", win: () => alert(`${players.player1.name} Wins!`)},
-    player2: {name: window.prompt("player2","player2").valueOf, marker: "O", win: () => alert(`${players.player2.name} Wins!`)},
-}
+  player1: {
+    name: window.prompt("Enter Player 1 name:", "X"), 
+    marker: "X", 
+    win: function() { gameboard.statusDiv.innerText = `${this.name} Wins!`; }
+  },
+  player2: {
+    name: window.prompt("Enter Player 2 name:", "O"), 
+    marker: "O", 
+    win: function() { gameboard.statusDiv.innerText = `${this.name} Wins!`; }
+  }
+};
 
-var game = {
+// game setup
+let gameState = ["","","","","","","","",""];
+let currentPlayer = players.player1;
+
+var gameboard = {
     accessDom: function() {
         this.header = document.getElementById("header");
         this.display = document.getElementById("display");
         this.board = document.getElementById("gameboard");
-        this.status = document.getElementById("status");
-        this.resetButton = document.getElementById("resetButton")
+        this.statusDiv = document.getElementById("status");
+        this.resetButton = document.getElementById("resetButton");
+        this.gameboardsquare = document.querySelectorAll(".cell");
     },
 
-    init: for (let square in gameboard) {
-        var gamesquare = document.createElement("div");
-        gamesquare.setAttribute("id", square);
-        board.appendchild(gamesquare);     
+    init: function() {
+        this.gameboardsquare.forEach(cell => cell.addEventListener("click", this.cellClick));
+        this.resetButton.addEventListener("click", this.restartGame);
+        this.statusDiv.innerText = `${players.player1.name}'s turn`;
     },
 
-    buttons: function() {
-        resetButton.onclick = game.init()
+    wincons: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ],
+
+    winCheck: function() {
+        let roundWon = false;
+        for(let i = 0; i < this.wincons.length; i++) {
+            const condition = this.wincons[i];
+            const cellA = gameState[condition[0]];
+            const cellB = gameState[condition[1]];
+            const cellC = gameState[condition[2]];
+            
+            if (cellA === "" || cellB === "" || cellC === "") {
+                continue;
+            }
+            if (cellA === cellB && cellB === cellC) {
+                roundWon = true;
+                break;
+            }
+
+        }
+        if(roundWon){
+            currentPlayer.win();
+        }
+        else if(!gameState.includes("")){
+            gameboard.statusDiv.innerText = `Draw. Play again?`            
+        }
+        else {currentPlayer = currentPlayer === players.player1 ? players.player2 : players.player1;
+                gameboard.statusDiv.innerText = `${currentPlayer.name}'s turn`;}
     },
 
-    playGame: function() {
+    cellClick: function(event) {
+        const cellIndex = this.getAttribute("cellIndex");
+        if (gameState[cellIndex] === "") {
+            gameState[cellIndex] = currentPlayer.marker;
+            gameboard.gameboardsquare[cellIndex].classList.add(currentPlayer === players.player1 ? 'player1-marker' : 'player2-marker');
+            gameboard.gameboardsquare[cellIndex].textContent = currentPlayer.marker;
+            gameboard.winCheck();
+    }
         
-    },
+},
 
-    winner: function() {
-        if (gameboard.One & gameboard.Two & gameboard.Three === players.player1.marker) {players.player1.win
-        } else if (gameboard.One & gameboard.Five & gameboard.Nine === players.player1.marker) {players.player1.win
-        } else if (gameboard.One & gameboard.Four & gameboard.Seven === players.player1.marker) {players.player1.win
-        } else if (gameboard.Two & gameboard.Five & gameboard.Eight === players.player1.marker) {players.player1.win
-        } else if (gameboard.Three & gameboard.Six & gameboard.Nine === players.player1.marker) {players.player1.win
-        } else if (gameboard.Three & gameboard.Five & gameboard.Seven === players.player1.marker) {players.player1.win
-        } else if (gameboard.Four & gameboard.Five & gameboard.Six === players.player1.marker) {players.player1.win
-        } else if (gameboard.Seven & gameboard.Eight & gameboard.Nine === players.player1.marker) {players.player1.win
-        } else if (gameboard.One & gameboard.Two & gameboard.Three === players.player2.marker) {players.player2.win
-        } else if (gameboard.One & gameboard.Five & gameboard.Nine === players.player2.marker) {players.player2.win
-        } else if (gameboard.One & gameboard.Four & gameboard.Seven === players.player2.marker) {players.player2.win
-        } else if (gameboard.Two & gameboard.Five & gameboard.Eight === players.player2.marker) {players.player2.win
-        } else if (gameboard.Three & gameboard.Six & gameboard.Nine === players.player2.marker) {players.player2.win
-        } else if (gameboard.Three & gameboard.Five & gameboard.Seven === players.player2.marker) {players.player2.win
-        } else if (gameboard.Four & gameboard.Five & gameboard.Six === players.player2.marker) {players.player2.win
-        } else if (gameboard.Seven & gameboard.Eight & gameboard.Nine === players.player2.marker) {players.player2.win
-        } else if (gameboard.One & gameboard.Two & gameboard.Three & gameboard.Four & gameboard.Five & gameboard.Six & gameboard.Seven & gameboard.Eight & gameboard.Nine != "") {
-            alert("It's a draw. Game Over")};
+    restartGame: function() {
+        gameState = ["","","","","","","","",""];
+        gameboard.gameboardsquare.forEach(cell => {
+        cell.innerText = "";
+        cell.classList.remove('player1-marker', 'player2-marker');
+        currentPlayer = players.player1;
+        gameboard.statusDiv.innerText = `${currentPlayer.name}'s turn`;
+    }); 
+    },    
+
+    start: function() {
+        gameboard.accessDom();
+        gameboard.init();
     },
-}
+};
+gameboard.start();
+
